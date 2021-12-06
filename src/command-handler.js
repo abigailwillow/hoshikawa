e3fe0a2583f604e9a2991c04c9c9726d5eccd264
Abby <abbydiode@gmail.com>
2021-12-06 16:38:33 +0100

    Add error handling to command handler

--- src/command-handler.js
+++ src/command-handler.js
@@ -4,7 +4,12 @@ const path = require('path');
 module.exports.handle = interaction => {
     const command = path.join(__dirname, 'commands', interaction.commandName + '.js');
     if (file.existsSync(command)) {
-        require(command).handle(interaction);
+        try {
+            require(command).handle(interaction);
+        } catch (error) {
+            console.error(error);
+            interaction.reply('Something went wrong while executing the command: ' + error.message);
+        }
     } else {
         interaction.reply('Sorry, I could not figure out how to handle your command');
     }
