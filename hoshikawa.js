31291794215c46f5209eb57ba0bb6bcea93e1e9d
Abby <abbydiode@gmail.com>
2021-12-06 20:07:37 +0100

    Implement userinfo command

--- hoshikawa.js
+++ hoshikawa.js
@@ -14,17 +14,6 @@ const client = new Client({
 	] 
 });
 
-// command.registerCommand('userinfo', (msg, user) => {
-// 	let member = getMember(msg.guild, user)
-
-// 	if (member != null) {
-// 		msg.channel.send(`**${member.displayName}** joined on \`${member.joinedAt.toISOString().substring(0, 10)}\``)
-// 	} else {
-// 		msg.channel.send(strings.error_no_user)
-// 	}
-// })
-
-// Events
 client.once('ready', () => {
 	console.log(`Logged in as ${client.user.tag}`);
 	console.log(`Currently serving ${client.users.cache.size} users`);
@@ -54,30 +43,4 @@ client.on('applicationCommandCreate', command => {
 	console.log(command);
 })
 
-function getUser(identifier) {
-	if (identifier instanceof User) {
-		return identifier
-	} else {
-		identifier = identifier.toLowerCase()
-		return client.users.find(x => x.id === identifier || x.username.toLowerCase().includes(identifier))
-	}
-}
-
-function randomInt(min, max) {
-	return Math.round(Math.random() * (max - min) + min)
-}
-
-function randomFloat(min, max) {
-	return Math.random() * (max - min) + min
-}
-
-function pluralize(word, count) {
-	if (Math.abs(count) != 1) { return word + 's' }
-	else { return word }
-}
-
-function replaceVar(str, arg) {
-	return str.replace(/%\w+%/g, arg)
-}
-
-client.login(config.token)
+client.login(config.token);
--- src/commands/userinfo.js
+++ src/commands/userinfo.js
@@ -1,3 +1,38 @@
-module.exports.handle = interaction => {
+let { MessageEmbed } = require('discord.js');
 
+module.exports.handle = interaction => {
+    // Dirty hack to check if mentionable is a role or not
+    if (!interaction.options.getMentionable('user').name) {
+        let embed = new MessageEmbed();
+
+        if (interaction.inGuild()) {
+            const member = interaction.options.getMentionable('user') ?? interaction.member;
+            embed.setAuthor(`${member.displayName}`, member.displayAvatarURL({size: 32}))
+            .setColor(member.displayHexColor);
+            embed.addFields([
+                {name: 'Discord ID', value: member.id, inline: true},
+                {name: 'Discord Tag', value: member.user.tag, inline: true},
+                {name: 'Account Creation Date', value: member.user.createdAt.toLocaleString(), inline: true},
+                {name: 'Join Date', value: member.joinedAt.toLocaleString(), inline: true},
+                {name: 'Bot', value: member.user.bot ? 'Yes' : 'No', inline: true},
+                {name: 'Roles', value: member.roles.cache.map(role => role.toString()).join(' '), inline: true},
+                {name: 'Avatar URL', value: member.user.displayAvatarURL({ size: 4096 }), inline: true}
+            ]);
+        } else {
+            const user = interaction.options.getMentionable('user') ?? interaction.user;
+            embed.setAuthor(`${user.username}`, user.displayAvatarURL({size: 32}))
+            .setColor(user.hexAccentColor);
+            embed.addFields([
+                {name: 'Discord ID', value: user.id, inline: true},
+                {name: 'Discord Tag', value: user.tag, inline: true},
+                {name: 'Account Creation Date', value: user.createdAt.toLocaleString(), inline: true},
+                {name: 'Bot', value: user.bot ? 'Yes' : 'No', inline: true},
+                {name: 'Avatar URL', value: user.displayAvatarURL({ size: 4096 })}
+            ]);
+        }
+
+        interaction.reply({ embeds: [embed] });
+    } else {
+        interaction.reply('Could not retrieve user info, did you mention a role by any chance?');
+    }
 };
