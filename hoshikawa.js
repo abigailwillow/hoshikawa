5a0bb7a1cd231218aad56ddddfbc0a4f0472131e
Abigail de Joode <abbydiode@gmail.com>
2022-04-20 22:46:11 +0200

    Update regex to be less greedy

--- hoshikawa.js
+++ hoshikawa.js
@@ -45,7 +45,7 @@ client.on('applicationCommandCreate', command => {
 });
 
 client.on('messageCreate', message => {
-	if (message.content.toLowerCase().replace(/[^\w\s]/g, '').match(/\bim?.+ga+y\b/g)) {
+	if (message.content.toLowerCase().replace(/[^\w\s]/g, '').match(/\bi[\w ]{0,24}ga+y\b/g)) {
 		message.reply('We know');
 	}
 });
