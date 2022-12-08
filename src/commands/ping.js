1338f1f8c7715e7fa01974957218bae82c06199d
abbydiode <contact@abbydiode.com>
2022-12-08 23:22:38 +0100

    Fix the ping command not working

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,7 +1,7 @@
 module.exports.handle = interaction => {
 	interaction.reply(`Latency to Discord is ${Math.round(interaction.client.ws.ping)}ms`);
     interaction.fetchReply().then(reply => {
-        const latency = new Date(reply.timestamp).getTime() - interaction.createdTimestamp;
+        const latency = new Date(reply.createdTimestamp).getTime() - interaction.createdTimestamp;
         interaction.editReply(reply.content + `, latency to Hoshikawa's server is ${latency}ms`);
     });
 };
