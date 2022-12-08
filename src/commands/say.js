6eab3626805597d89d544ed184a0dd9befb9c40a
abbydiode <contact@abbydiode.com>
2022-12-08 15:43:19 +0100

    Add override for operators to use admin commandd

--- src/commands/say.js
+++ src/commands/say.js
@@ -4,7 +4,7 @@ module.exports.handle = interaction => {
     const channel = interaction.options.getChannel('channel');
     const message = interaction.options.getString('message');
     if (channel.isText()) {
-        interaction.reply('✅');
+        interaction.reply(`✅ Sent message to ${message} ${channel}`);
         channel.send(message);
     } else {
         interaction.reply(localization.error_no_text_channel);
